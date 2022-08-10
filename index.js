52098952c636878aa803cfa9dc2975bebe52992a
BrandtD <brandtdavidson@gmail.com>
2022-08-10 10:24:49 -0600

    resolving depend. issues, and deprecated packages

---
+++ index.js
@@ -0,0 +1,11 @@
+// Including packages needed and importing the given tests for employee, engineer, intern, and manager
+const inquirer = require("inquirer");
+const fs = require('fs');
+const jest = require('jest');
+const Employee = require('./Develop/__tests__/Employee.test');
+const Engineer = require('./Develop/__tests__/Engineer.test');
+const Intern = require('./Develop/__tests__/Intern.test');
+const Manager = require('./Develop/__tests__/Manager.test');
+
+
+
